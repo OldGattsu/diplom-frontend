4802ae4710e11e270b199f4ba0a230354a439d8a
Artyom <krasavin-00@mail.ru>
2023-01-24 23:33:42 +0300

    init

---
+++ app.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="white"
+    >
+      <v-container class="py-0 fill-height">
+        <v-avatar
+          class="mr-10"
+          color="grey darken-1"
+          size="32"
+        ></v-avatar>
+
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          text
+        >
+          {{ link }}
+        </v-btn>
+
+        <v-spacer></v-spacer>
+      </v-container>
+    </v-app-bar>
+
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <v-col>
+            <v-sheet
+              rounded="lg"
+            >
+              <v-card
+                width="400"
+                title="This is a title"
+                subtitle="This is a subtitle"
+                text="This is content"
+              ></v-card>
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+const links = [
+  'Dashboard',
+  'Messages',
+  'Profile',
+  'Updates',
+]
+</script>
